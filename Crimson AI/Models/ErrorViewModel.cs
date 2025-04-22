495ec0bb3a84a3062fc57a71860060b04315d78a
laurynharvey127 <laurynharvey5@gmail.com>
2025-04-22 13:36:24 -0500

    Add files via upload

---
+++ Crimson AI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Crimson_AI.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
